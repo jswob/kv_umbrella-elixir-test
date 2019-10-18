006d3064b610507ea9b21dd09707e0372d4e249b
jacek swoboda <jacswob@gmail.com>
2019-10-18 07:14:40 +0200

    8. Task and gen_tcp

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -4,7 +4,13 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
-    children = []
+    port =
+      String.to_integer(System.get_env("PORT") || raise("missing $PORT environment variable"))
+
+    children = [
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
+    ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
